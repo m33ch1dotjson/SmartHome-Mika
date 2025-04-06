fb7b4c89a1fe39e66300f89012a053ca25f4d941
Demi <Demi@DESKTOP-K01F946>
2025-04-06 14:53:12 +0200

    Add project files.

---
+++ SmartHome Mika/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SmartHome_Mika.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
